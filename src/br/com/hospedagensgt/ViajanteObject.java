e1485f373e3cf500a026abade65d9772b9044bad
Eduardo F Zimmermann <edu.zimmermann.efz@gmail.com>
2022-04-26 22:47:42 -0300

    Tentativa de implementação do ArrayList na classe Viajante

---
+++ src/br/com/hospedagensgt/ViajanteObject.java
@@ -0,0 +1,20 @@
+package br.com.hospedagensgt;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+public class ViajanteObject extends Viajante{
+
+	public static void main(String[] args) {
+		
+		Scanner leitor = new Scanner(System.in);
+		
+		ArrayList <Viajante)> viajante = new ArrayList();		
+		viajante.add();
+		
+
+	}
+
+}
